12412601b51d1e30126fe7952ccda71a83b24c26
Hack With Rohit Tamil <162045216+Hack-With-Rohith-tamil@users.noreply.github.com>
2024-04-19 17:35:55 +0530

    KALI_LINUX_MAC_CHANGER.py

--- KALI_LINUX_MAC_CHANGER.py
+++ KALI_LINUX_MAC_CHANGER.py
@@ -4,9 +4,9 @@
 import subprocess 
 import pyfiglet 
 
-msg = pyfiglet.figlet_format("MAC spoofing is RAM !!!!")
+msg = pyfiglet.figlet_format("MAC spoofing is a Home Address !!!!")
 print(msg)
-i=input("Enter SSD to Fast Spoof : ")
+i=input("Enter New Home Address to Fast Spoof : ")
 subprocess.call(["ifconfig","wlan0","down"])
 subprocess.call(["ifconfig","wlan0","hw","ether",i])
 subprocess.call(["ifconfig","wlan0","up"])
